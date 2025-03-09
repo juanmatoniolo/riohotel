39477bd81c15db2522f1d9294249d7414ffc34c9
juanmatoniolo <juanmatoniolo2@gmail.com>
2025-03-09 17:23:31 -0300

    termine el header

--- src/pages/Destinos/Destinos.jsx
+++ src/pages/Destinos/Destinos.jsx
@@ -4,96 +4,10 @@ import imagenes from "../../assets/Imagenes";
 
 const Destinos = () => {
 	return (
-		<div className="elementor-681">
-			{/* Sección con imagen de fondo y overlay */}
-
-			{/* Sección de Santa Ana */}
-			<div className="elementor-element elementor-element-19bd2ce">
-				<div className="elementor-container">
-					<div className="elementor-column elementor-col-100">
-						<div className="elementor-widget-wrap">
-							{/* Título */}
-
-							{/* Subtítulo */}
-							<div className="elementor-element elementor-element-624003d">
-								<h2 className="elementor-heading-title">
-									SANTA ANA
-								</h2>
-							</div>
-							{/* Descripción */}
-							<div className="elementor-element elementor-element-c612b87">
-								<p>
-									Zona de playas y camping, rodeadas de
-									naturaleza y costas de arena blanca.
-								</p>
-							</div>
-							{/* Galería */}
-							<div className="elementor-element elementor-element-daab2d8">
-								<div className="elementor-gallery__container">
-									{/* Imágenes de la galería */}
-									{[1, 2, 3, 4].map((item) => (
-										<a
-											key={item}
-											className="e-gallery-item"
-											href={`#img${item}`}
-										>
-											<div className="e-gallery-image"></div>
-											<div className="elementor-gallery-item__overlay"></div>
-										</a>
-									))}
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			{/* Sección de Termas */}
-			<div className="elementor-element elementor-element-19bd2ce">
-				<div className="elementor-container">
-					<div className="elementor-column elementor-col-100">
-						<div className="elementor-widget-wrap">
-							{/* Título */}
-							<div className="elementor-element elementor-element-ed3a3ff">
-								<h6 className="elementor-heading-title">
-									Explora Entre Rios
-								</h6>
-							</div>
-							{/* Subtítulo */}
-							<div className="elementor-element elementor-element-7272304">
-								<h2 className="elementor-heading-title">
-									TERMAS
-								</h2>
-							</div>
-							{/* Descripción */}
-							<div className="elementor-element elementor-element-7721733">
-								<p>
-									En el complejo termal de Chajarí podrán
-									relajarse y disfrutar de las diversas
-									piscinas de aguas termales.
-								</p>
-							</div>
-							{/* Galería */}
-							<div className="elementor-element elementor-element-993e854">
-								<div className="elementor-gallery__container">
-									{/* Imágenes de la galería */}
-									{[1, 2, 3, 4].map((item) => (
-										<a
-											key={item}
-											className="e-gallery-item"
-											href={`#img${item}`}
-										>
-											<div className="e-gallery-image"></div>
-											<div className="elementor-gallery-item__overlay"></div>
-										</a>
-									))}
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+	<>
+  
+  
+  </>
 	);
 };
 
--- src/pages/Navv/Nav.jsx
+++ src/pages/Navv/Nav.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { FaBars } from "react-icons/fa";
-import "../../styles/styles.css";
+import { FaBars, FaMapMarkerAlt, FaPhone } from "react-icons/fa"; // Importa los íconos
+import "./navbar.css";
 import imagenes from "../../assets/Imagenes";
-
 
 function Navegacion() {
 	const [expanded, setExpanded] = useState(false);
 
+	// Función para manejar los clics en los enlaces
+	const handleClick = (url) => {
+		window.open(url, "_blank"); // Abre el enlace en una nueva pestaña
+	};
+
 	return (
-		<>
 		<Navbar
 			expand="sm"
 			className="navbar-custom"
 			expanded={expanded}
 			onToggle={() => setExpanded(!expanded)}
 		>
+			
 			<Container>
-				<Navbar.Brand href="" className="titulo-nav"><img src={imagenes.riohetel2} alt="" /></Navbar.Brand>
+				<Navbar.Brand href="" className="titulo-nav">
+					<img src={imagenes.riohetel2} alt="Hotel Río" />
+				</Navbar.Brand>
 				<Navbar.Toggle
 					aria-controls="navbarsExample03"
 					className={`custom-toggler ${expanded ? "rotated" : ""}`}
@@ -26,19 +32,40 @@ function Navegacion() {
 					<FaBars />
 				</Navbar.Toggle>
 				<Navbar.Collapse id="navbarsExample03">
-					<Nav className="me-auto">
-						<Nav.Link href="#comodidades">Comodidades</Nav.Link>
-						<Nav.Link href="#habitaciones">Habitaciones</Nav.Link>
-						<Nav.Link href="#destinos">Turismo local</Nav.Link>
+					<Nav className="me-auto controlador-navbar">
+						{/* Sección de enlaces principales */}
+						<div className="primeros-link links-nav">
+							<Nav.Link href="#comodidades">Comodidades</Nav.Link>
+							<Nav.Link href="#habitaciones">
+								Habitaciones
+							</Nav.Link>
+							<Nav.Link href="#destinos">Turismo local</Nav.Link>
+						</div>
+
+						{/* Sección de enlaces con iconos UX/UI */}
+						<div className="segundos-link links-nav">
+							<Nav.Link
+								onClick={() =>
+									handleClick(
+										"https://www.google.com/maps/dir//Rivadavia+4085,+Chajarí,+Entre+Ríos"
+									)
+								}
+							>
+								<FaMapMarkerAlt style={{ color: "#24B3A1" }}  /> Cómo llegar
+							</Nav.Link>
+							<Nav.Link
+								onClick={() =>
+									handleClick("https://wa.me/543456496264")
+								}
+							>
+								<FaPhone style={{ color: "#24B3A1" }} /> Contacto
+
+							</Nav.Link>
+						</div>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
-
-
-
-
-</>
 	);
 }
 
